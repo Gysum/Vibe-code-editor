74ad5be5717e44f504551259a41d60e67ac1df34
Gysum <aakashpathrikar@gmail.com>
2026-07-21 17:04:42 +0530

    database setup

--- app/page.tsx
+++ app/page.tsx
@@ -1,7 +1,9 @@
 import Image from "next/image";
 import {Button} from "@/components/ui/button"
+import { db } from "@/lib/db";
 
-export default function Home() {
+export default async function Home() {
+const user = db.user
   return (
     <div className="flex flex-col justify-center items-center h-screen bg-gray-100">
       <Button>
